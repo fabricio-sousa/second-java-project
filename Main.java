29a273fa474c8b6d96610470dee113b44e36ad21
Fabricio Sousa <fabriciosousa@Mobiworks-iMac-2.local>
2018-07-05 11:25:54 -0400

    Create hello method in Person class.

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ class Main {
   public static void main(String[] args) {
     Person person1 = new Person();
     // Call the hello method using the person1 instance
-    
+    person1.hello();
     
     Person person2 = new Person();
     // Call the hello method using the person2 instance
-    
+    person2.hello();
     
   }
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,6 @@
 class Person {
   // Define the hello instance method
-  
-  
+  public void hello() {
+      System.out.println("Hello");
+    }
 }
