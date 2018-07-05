dc80561a371ba070657f6fbfc8c7b30c50842b71
Fabricio Sousa <fabriciosousa@Mobiworks-iMac-2.local>
2018-07-05 11:21:17 -0400

    First commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person();
+    // Call the hello method using the person1 instance
+    
+    
+    Person person2 = new Person();
+    // Call the hello method using the person2 instance
+    
+    
+  }
+}
---
+++ Person.java
@@ -0,0 +1,5 @@
+class Person {
+  // Define the hello instance method
+  
+  
+}
