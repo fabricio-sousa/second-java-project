36c386428abe3516bd99457b71bd43612d7946cc
Fabricio Sousa <fabriciosousa@Mobiworks-iMac-2.local>
2018-07-10 09:40:08 -0400

    Define Class variables

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 class Main {
   public static void main(String[] args) {
     Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
-
-    System.out.println(person1.fullName());
-    System.out.println(person1.age);
-    System.out.println(person1.bmi());
+    person1.printData();
+    
+    Person person2 = new Person("John", "Smith", 65, 1.75, 80.0);
+    person2.printData();
+    
+    // Output “Total: ____ people.”
+    System.out.println("Total: " + Person.count + " people.");
+    
   }
 }
